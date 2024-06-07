a4fc09f64d232e8b8b4059a33cf4a5965009cf65
sunnysinghaur <sunnysinghaur8298@gmail.com>
2024-06-07 16:28:45 +0530

    Add files via upload

---
+++ emailotp.js
@@ -0,0 +1,37 @@
+function sendOTP() {
+	const email = document.getElementById('email');
+	const otpverify = document.getElementsByClassName('otpverify')[0];
+
+	let otp_val = Math.floor(Math.random() * 10000);
+
+	let emailbody = `<h2>Your OTP is </h2>${otp_val}`;
+	Email.send({
+		SecureToken : "95cfd9f7-361e-4358-96f0-197a4da818e5",
+		To : email.value,
+		From : "sunnysinghaur8298@gmail.com",
+		Subject : "email OTP using javascript",
+		Body : emailbody,
+	}).then(
+	  
+	
+
+	message => {
+		if (message === "OK") {
+			alert("OTP sent to your email " + email.value);
+
+			otpverify.style.display = "flex";
+			const otp_inp = document.getElementById('otp_inp');
+			const otp_btn = document.getElementById('otp-btn');
+
+			otp_btn.addEventListener('click', () => {
+				if (otp_inp.value == otp_val) {
+					alert("Email address verified...");
+				}
+				else {
+					alert("Invalid OTP");
+				}
+			})
+		}
+	}
+);
+}
